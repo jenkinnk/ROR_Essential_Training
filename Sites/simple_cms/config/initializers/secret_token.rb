14192ced0afcd867f50d10e4251ff2628e698983
jenkinnk <jenkinnk@gmail.com>
2016-02-03 10:15:55 -0500

    First commit
    Rails project initialized using mysql as database manager

---
+++ Sites/simple_cms/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SimpleCms::Application.config.secret_key_base = '21042e6990fa7f4a27ee9875aebab2a7511e712291e209320cbff4370c4a82bb9c464e19940f8fed0ea3b2a61c3340ee8d92faaef5a6e251491eabea43c48e50'

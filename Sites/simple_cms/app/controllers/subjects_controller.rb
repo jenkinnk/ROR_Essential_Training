d025aa70063ff9656a8884f18e843298456b07f3
jenkinnk <jenkinnk@gmail.com>
2016-02-04 12:20:01 -0500

    Dynamic Views Created
    Views were added and updated to allow the user to specific which record
    they intend to access. A list of records was also presented to the user
    from the database table.

--- Sites/simple_cms/app/controllers/subjects_controller.rb
+++ Sites/simple_cms/app/controllers/subjects_controller.rb
@@ -3,9 +3,11 @@ class SubjectsController < ApplicationController
   layout false
   
   def index
+	@subjects = Subject.sorted
   end
 
   def show
+	@subject = Subject.find(params[:id])
   end
 
   def new

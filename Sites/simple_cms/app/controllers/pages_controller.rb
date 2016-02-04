e672d90dac6c8b5907ff893d0afcbff15f39a06a
jenkinnk <jenkinnk@gmail.com>
2016-02-04 15:06:19 -0500

    Pages functionality added to complete coding challenge
    The CRUD operations for the Pages class were added. The HTML for the
    pages was added to build the UI.

--- Sites/simple_cms/app/controllers/pages_controller.rb
+++ Sites/simple_cms/app/controllers/pages_controller.rb
@@ -3,27 +3,53 @@ class PagesController < ApplicationController
   layout false
   
   def index
-	@pages = Page.sorted
+	@pages=Page.sorted
   end
 
   def show
+	@page=Page.find(params[:id])
   end
 
   def new
+	@page=Page.new()
   end
 
   def create
+	@page = Page.new(page_params)
+	if @page.save
+		flash[:notice] = "Page Created Successfully"
+		redirect_to(:action => 'index')
+	else
+		render('new')
+	end
   end
 
   def edit
+	@page=Page.find(params[:id])
   end
 
   def update
+	@page=Page.find(params[:id])
+	if @page.update_attributes(page_params)
+		flash[:notice] = "Page Updated Successfully"
+		redirect_to(:action => 'show', :id=>@page.id)
+	else
+		render('new')
+	end
   end
 
   def delete
+	@page=Page.find(params[:id])
   end
 
   def destroy
+	page=Page.find(params[:id]).destroy
+	flash[:notice] = "Page '#{page.name}' Deleted  Successfully"
+	redirect_to(:action => 'index')
   end
+  
+    private 
+	def page_params
+		params.require(:page).permit(:name, :permalink, :position, :visible)
+	end
 end

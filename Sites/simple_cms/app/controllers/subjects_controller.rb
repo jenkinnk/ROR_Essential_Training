5f7924d3197eef6146785dca988103746a7aa8db
jenkinnk <jenkinnk@gmail.com>
2016-02-04 13:16:01 -0500

    Flash Hash Messages
    The user interface was enhanced by adding a flash message to confirm
    when actions were completed.

--- Sites/simple_cms/app/controllers/subjects_controller.rb
+++ Sites/simple_cms/app/controllers/subjects_controller.rb
@@ -18,6 +18,7 @@ class SubjectsController < ApplicationController
 	#Instantiate a new object
 	@subject = Subject.new(subject_params)
 		if @subject.save #save the object
+			flash[:notice] = "Subject Created Successfully"
 			redirect_to(:action => 'index') #redirect to the index page
 		else
 			render('new') #if save fails, allow the user to update info
@@ -26,12 +27,12 @@ class SubjectsController < ApplicationController
 
   def edit
 	@subject = Subject.find(params[:id])
-		
   end
   
   def update
 	@subject = Subject.find(params[:id])
 		if @subject.update_attributes(subject_params)
+			flash[:notice] = "Subject Updated Successfully"
 			redirect_to(:action => 'show', :id=>@subject.id) #redirect to the index page
 		else
 			render('edit') #if update fails, allow the user to update info
@@ -39,6 +40,13 @@ class SubjectsController < ApplicationController
   end
 
   def delete
+	@subject = Subject.find(params[:id])
+  end
+  
+  def destroy
+	subject = Subject.find(params[:id]).destroy
+	flash[:notice] = "Subject '#{subject.name}' Deleted Successfully"
+	redirect_to(:action => 'index')
   end
   
   private 

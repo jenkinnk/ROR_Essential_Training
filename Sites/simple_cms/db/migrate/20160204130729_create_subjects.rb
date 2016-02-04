33c1093b2c08a836767163fefe5aa8d45b57600b
jenkinnk <jenkinnk@gmail.com>
2016-02-04 09:08:29 -0500

    Migration Files for Coding Challenge
    Migration files created to create and alter tables within the database.

--- Sites/simple_cms/db/migrate/20160204130729_create_subjects.rb
+++ Sites/simple_cms/db/migrate/20160204130729_create_subjects.rb
@@ -2,8 +2,8 @@ class CreateSubjects < ActiveRecord::Migration
   def change
     create_table :subjects do |t|
       t.string :name
-      t.int :position
-      t.boolean :visible
+      t.integer :position
+      t.boolean :visible, :default=>false
 
       t.timestamps null: false
     end
--- Sites/simple_cms/db/migrate/20160204130810_create_pages.rb
+++ Sites/simple_cms/db/migrate/20160204130810_create_pages.rb
@@ -1,12 +1,15 @@
 class CreatePages < ActiveRecord::Migration
   def change
     create_table :pages do |t|
-      t.string :name
+	  t.integer :subject_id
+	  t.string :name
       t.string :permalink
-      t.int :position
-      t.boolean :visible
+      t.integer :position
+      t.boolean :visible, :default=>false
 
       t.timestamps null: false
     end
+	add_index("pages", "subject_id")
+	add_index("pages", "permalink")
   end
 end

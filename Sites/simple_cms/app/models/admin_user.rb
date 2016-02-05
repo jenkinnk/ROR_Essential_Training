3b8b3a561a6c5ed509d848a1274eec0d9c211579
jenkinnk <jenkinnk@gmail.com>
2016-02-05 10:32:52 -0500

    Password Authentication Added
    A migration was created to create a hashed password column in the
    database. A validation statement was also added to the admin_user class.
    The authentication gem bcrypt was added to the application. Other
    dependencies were updated to fix compilation errors.

--- Sites/simple_cms/app/models/admin_user.rb
+++ Sites/simple_cms/app/models/admin_user.rb
@@ -3,6 +3,9 @@ class AdminUser < ActiveRecord::Base
 	# Class and file name were changed instead
 	#self.table_name = "admin_users"
 	
+	#Authentication
+	has_secure_password
+	
 	# Validation
 	EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
 	FORBIDDEN_USERNAMES = ['littlebopeep','humptydumpty','marymary']
@@ -19,13 +22,15 @@ class AdminUser < ActiveRecord::Base
 	#validates_confirmation_of :email
 	
 	#'Sexy Validation'
-	validates :first_name, presence => true, :length => {:maximum => 25}
-	validates :last_name, presence => true, :length => {:maximum => 50}
-	validates :username, presence => true, :length => {:within => 8..25}, uniqueness=>true
-	validates :email, presence => true, :length => {:maximum => 100}, :format=> EMAIL_REGEX, :confirmation=>true
+	validates :first_name, :presence => true, :length => {:maximum => 25}
+	validates :last_name, :presence => true, :length => {:maximum => 50}
+	validates :username, :presence => true, :length => {:within => 8..25}, :uniqueness=>true
+	validates :email, :presence => true, :length => {:maximum => 100}, :format=> EMAIL_REGEX, :confirmation=>true
 	
 	validate :username_is_allowed
 	#validate :no_new_users_on_saturday, :on => :create
+	
+	
 	
 	def username_is_allowed
 		if FORBIDDEN_USERNAMES.include?(username)

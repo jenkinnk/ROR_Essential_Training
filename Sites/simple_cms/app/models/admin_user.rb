bdf634ad136b7b45d71103c2d62e110db053f7e4
jenkinnk <jenkinnk@gmail.com>
2016-02-05 08:36:44 -0500

    Custom Validations
    Two custom validation methods were added to the adminuser class.

--- Sites/simple_cms/app/models/admin_user.rb
+++ Sites/simple_cms/app/models/admin_user.rb
@@ -5,6 +5,7 @@ class AdminUser < ActiveRecord::Base
 	
 	# Validation
 	EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
+	FORBIDDEN_USERNAMES = ['littlebopeep','humptydumpty','marymary']
 	#validates_presence_of :first_name
 	#validates_length_of :first_name, :maximum => 25
 	#validates_presence_of :last_name
@@ -22,6 +23,21 @@ class AdminUser < ActiveRecord::Base
 	validates :last_name, presence => true, :length => {:maximum => 50}
 	validates :username, presence => true, :length => {:within => 8..25}, uniqueness=>true
 	validates :email, presence => true, :length => {:maximum => 100}, :format=> EMAIL_REGEX, :confirmation=>true
+	
+	validate :username_is_allowed
+	#validate :no_new_users_on_saturday, :on => :create
+	
+	def username_is_allowed
+		if FORBIDDEN_USERNAMES.include?(username)
+			errors.add(:username, "has been restricted from use")
+		end
+	end
+	
+	def no_new_users_on_saturday
+		if Time.now.wday == 6
+			errors[:base] << "No new users on Saturdays"
+		end
+	end
 	
 	#Relationships
 	has_and_belongs_to_many :pages
